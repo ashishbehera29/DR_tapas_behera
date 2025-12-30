abab47789192e426b3efff2bc5bf6607e1d7debf
ashishbehera29 <ashishbehera8382@gmail.com>
2025-12-30 20:04:23 +0530

    1

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Target, Eye } from 'lucide-react';
 
 export default function About() {
+  //name
   return (
     <section id="about" className="py-16 md:py-24 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
